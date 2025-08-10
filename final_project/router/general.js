4b33c0fff3baaf9cb58e7537ee3acaba37331157
gaulduer <gaulduer@gmail.com>
2025-08-10 12:01:21 -0400

    axios requests

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,20 +24,50 @@ public_users.post("/register", (req,res) => {
     return res.status(404).send("Unable to register user. Missing username or password");
 });
 
-public_users.get('/',function (req, res) {
-  return res.status.send('Books:\n' + JSON.stringify(books, null, 4) + "\n");
+public_users.get('/', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+    return res.status(200).send((await response.data));
+  }
+  catch(e) {
+    return res.status(404).send('Unable to obtain books.\n');
+  };
 });
 
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/books', function (req, res) {
+  return res.status(200).send(("Books:\n" + JSON.stringify(books, null, 4) + "\n"));
+});
+
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/bookbyisbn/' + req.params.isbn);
+      return res.status(200).send((await response.data));
+    }
+    catch(e) {
+      return res.status(404).send('Unable to obtain book.\n');
+    };
+});
+
+public_users.get('/bookbyisbn/:isbn', function (req, res) {
   const book = books[req.params.isbn];
   
   if(!book)
-    return res.send("Unable to find book with provided isbn.");
+    return res.status(404).send("Unable to find book with provided isbn.");
   
   return res.status(200).send("Details for book with isbn " + req.params.isbn + ":\n" + JSON.stringify(book, null, 4) + "\n");
- });
+});
+
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/booksbyauthor/' + req.params.author);
+    return res.status(200).send((await response.data));
+  }
+  catch(e) {
+    return res.status(404).send('Unable to obtain book.\n');
+  };
+});
   
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/booksbyauthor/:author', function (req, res) {
     const author = req.params.author;
     const books_by_author = {};
 
@@ -49,7 +79,17 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(200).send("Books by " + author + ":\n" + JSON.stringify(books_by_author, null, 4) + "\n");
 });
 
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/booksbytitle/' + req.params.title);
+    return res.status(200).send((await response.data));
+  }
+  catch(e) {
+    return res.status(404).send('Unable to obtain book.\n');
+  };
+});
+
+public_users.get('/booksbytitle/:title', function (req, res) {
   const title = req.params.title;
   const books_by_title = {};
 
